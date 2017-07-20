6ef0db01066ae68ea6a682bc5221ea3035bf5fca
Usman Muhammad <Usman@Usmans-MacBook-Air.local>
2017-07-20 16:27:28 -0400

    Add GET /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,16 @@
 var mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var promise = mongoose.connect('mongodb://localhost:27017/TodoApp', {
+  useMongoClient: true,
+});
 
 module.exports = {mongoose};
+
+
+
+// var mongoose = require('mongoose');
+//
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+//
+// module.exports = {mongoose};
