3ff539c324abb96b9869971561f906994e9b7b2a
Usman Muhammad <Usman@Usmans-MacBook-Air.local>
2017-07-21 16:18:58 -0400

    error corrections

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 
-var promise = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
-  useMongoClient: true,
-});
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
 
