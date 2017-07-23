00aacb5431d082ef52c511547ea20b451be6e224
Usman Muhammad <Usman@Usmans-MacBook-Air.local>
2017-07-23 16:07:36 -0400

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -31,23 +32,22 @@ app.get('/todos', (req, res) => {
   });
 });
 
-//  GET /todos/123424
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-if (!ObjectID.isValid(id)) {
-  return res.status(404).send();
-}
-
-Todo.findById(id).then((todo) => {
-  if (!todo) {
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-res.send({todo});
-}).catch((e) => {
-  res.status(400).send();
-});
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.delete('/todos/:id', (req, res) => {
@@ -68,6 +68,31 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
